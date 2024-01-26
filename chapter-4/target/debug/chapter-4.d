41217df808623729f5820f036346d241631c5c3a
deechou <dchou1@gmail.com>
2024-01-26 18:35:48 -0500

    ownership practice

---
+++ chapter-4/target/debug/chapter-4.d
@@ -0,0 +1,1 @@
+C:\Users\echou\Documents\Projects\RustTutorials\rust-chapter-4\chapter-4\target\debug\chapter-4.exe: C:\Users\echou\Documents\Projects\RustTutorials\rust-chapter-4\chapter-4\src\main.rs
---
+++ chapter-4/target/debug/deps/chapter_4.d
@@ -0,0 +1,5 @@
+C:\Users\echou\Documents\Projects\RustTutorials\rust-chapter-4\chapter-4\target\debug\deps\chapter_4.exe: src\main.rs
+
+C:\Users\echou\Documents\Projects\RustTutorials\rust-chapter-4\chapter-4\target\debug\deps\chapter_4.d: src\main.rs
+
+src\main.rs:
