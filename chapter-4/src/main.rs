1d7fbf20581f9053f1592f249cf95b39219801b4
deechou <dchou1@gmail.com>
2024-01-28 13:50:29 -0500

    borrowing

--- chapter-4/src/main.rs
+++ chapter-4/src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
-    let s1 = String::from("hello");
+    let mut s = String::from("hello");
 
-    let len = calculate_length(&s1);
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
 
-    println!("The length of '{}' is {}.", s1, len);
-}
-
-fn calculate_length(s: &String) -> usize {
-    s.len()
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+    
 }
